6cf99cc0d0aa24f37f695a4e35997d3ea18f9178
Wanderson Castro <wandersonce1@hotmail.com>
2022-05-26 16:58:31 -0700

    Adding jobs and resume button

--- src/pages/Experience/index.tsx
+++ src/pages/Experience/index.tsx
@@ -25,10 +25,104 @@ export function Experience() {
                 <h2>EXPERIENCE</h2>
                 <p>Jobs So Far</p>
             </div>
+                <a target="_blank" href={require('../../assets/resume.pdf')} rel="noreferrer" >SEE RESUME</a>
         </div>
       </div>
       <div className="experience-wrapper">
+        <div>
+            <p className="company-name">General Internet</p>
+            <p className="position">Frontend Developer</p>
+            <p className="work-date">11/2021 – Now</p>
+        </div>
+        <div>
+            <ul>
+              <li>Build a custom WordPress Website based on the offered mockup.</li>
+              <li>Build and Customize ecommerces on Wordpress and Shopify.</li>
+              <li>Build Websites using HTML/CSS/JavaScript.</li>
+              <li>Web maintenance (updates) on previously built web pages.</li>
+              <li>Create and optimize content for websites using various graphics, databases, animations, and software.</li>
+              <li>Develop websites architecture and determine hardware and software requirements.</li>
+              <li> Research and evaluate a variety of interactive media products.</li>
+              <li> Conduct tests and perform security and quality assurance on new and existing web pages.</li>
+              <li> Setup DNS Server and SSL Certificates.</li>
+              <li> Deploy, Maintain, and move websites.</li>
+            </ul>
+        </div>
+      </div>
 
+      <div className="experience-wrapper">
+        <div>
+            <p className="company-name">Diluceo</p>
+            <p className="position">Web Developer -Freelancer</p>
+            <p className="work-date">06/2021 – Now</p>
+        </div>
+        <div>
+            <ul>
+              <li>Create custom code for responsive websites based on an approved mockup from the design team and client.</li>
+              <li>Build and Customize ecommerces on Wordpress and Shopify.</li>
+              <li>Build Websites using HTML/CSS/JavaScript.</li>
+              <li>Web maintenance (updates) on previously built web pages.</li>
+              <li>Create and optimize content for websites using various graphics, databases, animations, and software.</li>
+              <li>Develop websites architecture and determine hardware and software requirements.</li>
+            </ul>
+        </div>
+      </div>
+
+      <div className="experience-wrapper">
+        <div>
+            <p className="company-name">Flinnwest Solutions Inc. </p>
+            <p className="position">Web Developer</p>
+            <p className="work-date">08/2020 – Now</p>
+        </div>
+        <div>
+            <ul>
+              <li>Create custom code for websites based on an approved mockup from the design team and client.</li>
+              <li> Web maintenance (updates) on previously built web pages</li>
+              <li>Create and optimize content for websites using various graphics, databases, animations, and software.</li>
+              <li>Develop websites architecture and determine hardware and software requirements.</li>
+              <li> Research and evaluate a variety of interactive media products.</li>
+              <li> Conduct tests and perform security and quality assurance on new and existing web pages.</li>
+              <li> Setup DNS Server and SSL Certificates.</li>
+              <li> Deploy, Maintain, and move websites.</li>
+            </ul>
+        </div>
+      </div>
+
+      <div className="experience-wrapper">
+        <div>
+            <p className="company-name">Economix Supermarket</p>
+            <p className="position">IT Support Specialist</p>
+            <p className="work-date">02/2017 - 09/2017</p>
+        </div>
+        <div>
+            <ul>
+              <li>Documentation of incidents and problems solved on Help Desk System.</li>
+              <li>Data entry.</li>
+              <li>Configure computers and networks.</li>
+              <li>Oversee and support all IT problems in all sectors.</li>
+              <li> Guarantee that knowledge bases and online docs are updated.</li>
+              <li>Basic maintenance on printers.</li>
+              <li> Configuring Network Devices.</li>
+              <li> Tech support to client systems and office automation.</li>
+            </ul>
+        </div>
+      </div>
+
+      <div className="experience-wrapper">
+        <div>
+            <p className="company-name">Economix Supermarket</p>
+            <p className="position">IT Support Specialist</p>
+            <p className="work-date">02/2017 - 09/2017</p>
+        </div>
+        <div>
+            <ul>
+              <li>Documentation of incidents and problems solved on Help Desk System.</li>
+              <li>Data entry.</li>
+              <li>Configure computers and networks.</li>
+              <li>Assist IT support analysts in solving more complex issues.</li>
+              <li> Tech support to client systems and office automation.</li>
+            </ul>
+        </div>
       </div>
     </Container>
   )
--- src/pages/Experience/styles.ts
+++ src/pages/Experience/styles.ts
@@ -1,15 +1,17 @@
 import styled, { keyframes } from 'styled-components';
 
-import { fadeInUp, fadeInLeft } from 'react-animations';
+import { fadeInUp, fadeInLeft, fadeInRight } from 'react-animations';
 
 const fadeInUpAnimation = keyframes`${fadeInUp}`;
 const fadeInLeftAnimation = keyframes`${fadeInLeft}`;
+const fadeInRightAnimation = keyframes`${fadeInRight}`;
 
 export const Container = styled.main`
 display: flex;
 justify-content: center;
 align-items: center;
 flex-direction: column;
+max-width:1280px;
 
 margin: 60px auto;
 
@@ -60,5 +62,86 @@ justify-content:center;
     left:0;
     transform: translate(0, -50%);
   }
+
+  a{
+    padding: 10px 40px;
+    font-weight: bold;
+    text-transform: uppercase;
+    background-color:var(--orange);
+    transition: all 0.3s ease-in-out;
+    text-align: center;
+    margin-top:30px;
+    max-width: 220px;
+
+    &:hover{
+      filter: brightness(0.9);
+    }
+  }
+}
+
+.experience-wrapper{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 30px;
+  max-width: 1000px;
+
+  >div:first-child{
+    background-color: rgba(153,159,173,0.5);
+    padding: 30px;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    flex-basis:60%;
+    max-height: 50%;
+    margin-right: -80px;
+    animation: 1s ${fadeInLeftAnimation};
+  }
+
+  >div:last-child{
+    background-color: rgba(153,159,173,0.2);
+    padding: 30px 150px;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    animation: 1s ${fadeInRightAnimation};
+  }
+
+  .company-name, .position{
+    font-weight: 600;
+    font-size:1.2rem;
+  }
+
+  .work-date{
+    font-style: italic;
+    font-weight: 300;
+    padding-top:5px;
+  }
+
+  ul{
+    list-style-type: square;
+    list-style-position:inside;
+    li{
+        margin-bottom: 10px;
+    }
+  }
+}
+
+.experience-wrapper:nth-child(odd){
+flex-direction: row-reverse;
+
+>div:first-child{
+  margin-right: 0;
+  margin-left: -80px;
+  animation: 1s ${fadeInRightAnimation};
+}
+>div:last-child{
+  animation: 1s ${fadeInLeftAnimation};
+}
 }
 `;
