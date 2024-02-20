7070586441ab2d2255aa081e53b7968a8ae2bb10
Wanderson Castro <wandersonce1@hotmail.com>
2024-02-20 13:07:38 -0800

    new portfolio items

--- src/pages/Portfolio/portfolioItem.ts
+++ src/pages/Portfolio/portfolioItem.ts
@@ -2,176 +2,196 @@ export default [
   {
     title: 'BamGames Bio Link',
     img: require('../../assets/websites/bamgames.png'),
-    website:'https://bamgamesofc.vercel.app/'
+    website: 'https://bamgamesofc.vercel.app/',
   },
   {
     title: 'Bc Apartment Insiders',
     img: require('../../assets/websites/bcai.png'),
-    website:'https://bcapartmentinsider.com/'
+    website: 'https://bcapartmentinsider.com/',
   },
   {
     title: 'Bedrock-Service',
     img: require('../../assets/websites/bedrock.png'),
-    website:'https://bedrock-service.com/'
+    website: 'https://bedrock-service.com/',
   },
   {
     title: 'Beck Insurance',
     img: require('../../assets/websites/beckinsurance.png'),
-    website:'https://bcapartmentinsider.com/'
+    website: 'https://bcapartmentinsider.com/',
   },
   {
     title: 'Business OS',
     img: require('../../assets/websites/bos.png'),
-    website:'https://businessos.ca/'
+    website: 'https://businessos.ca/',
   },
   {
     title: 'B Smart Insurance',
     img: require('../../assets/websites/bsmart.png'),
-    website:'https://bsmartinsurance.com/'
+    website: 'https://bsmartinsurance.com/',
   },
   {
     title: 'Cameron Contracting',
     img: require('../../assets/websites/cameron.png'),
-    website:'https://cameroncontractingltd.ca/'
+    website: 'https://cameroncontractingltd.ca/',
   },
   {
     title: 'Canadian Business Ventures',
     img: require('../../assets/websites/cbv.png'),
-    website:'https://canadianbusinessventures.com/'
+    website: 'https://canadianbusinessventures.com/',
   },
   {
     title: 'David Bourchard Books',
     img: require('../../assets/websites/davidbou.png'),
-    website:'https://davidbouchardbooks.com/'
+    website: 'https://davidbouchardbooks.com/',
   },
   {
     title: 'DibFit',
     img: require('../../assets/websites/dibfit.png'),
-    website:'https://dibfit.com/'
+    website: 'https://dibfit.com/',
   },
   {
     title: 'Ethernal AI',
     img: require('../../assets/websites/ethernalai.png'),
-    website:'https://ethernal-ai.vercel.app/'
+    website: 'https://ethernal-ai.vercel.app/',
   },
   {
     title: 'Fixer Production',
     img: require('../../assets/websites/fixerProduction.png'),
-    website:'https://fixerproduction.com'
+    website: 'https://fixerproduction.com',
+  },
+  {
+    title: 'Insure Chat',
+    img: require('../../assets/websites/insureChat.png'),
+    website: 'https://insurechat.ca/',
+  },
+  {
+    title: 'Kirios Grafica',
+    img: require('../../assets/websites/kirios.png'),
+    website: 'https://kirios.com.br/',
   },
   {
     title: 'Grantson Construction',
     img: require('../../assets/websites/grantson.png'),
-    website:'https://www.grantsonconstruction.com/'
+    website: 'https://www.grantsonconstruction.com/',
   },
   {
     title: 'General Internet',
     img: require('../../assets/websites/gi.png'),
-    website:'https://generalinternet.ca'
+    website: 'https://generalinternet.ca',
   },
   {
     title: 'GI Training',
     img: require('../../assets/websites/giTraining.png'),
-    website:'https://gitraining.ca/'
+    website: 'https://gitraining.ca/',
   },
   {
     title: 'Linda Pet',
     img: require('../../assets/websites/lindapet.png'),
-    website:'https://www.lindayourpetnanny.ca/'
+    website: 'https://www.lindayourpetnanny.ca/',
   },
   {
     title: 'Merati Homes',
     img: require('../../assets/websites/merati.png'),
-    website:'https://www.meratihomes.com/'
+    website: 'https://www.meratihomes.com/',
   },
   {
     title: 'Ohh la la baby',
     img: require('../../assets/websites/ohhlala.png'),
-    website:'https://oohlalababy.ca/'
+    website: 'https://oohlalababy.ca/',
   },
   {
     title: 'Parabolic Marketing',
     img: require('../../assets/websites/parabolic.png'),
-    website:'https://parabolic.marketing'
+    website: 'https://parabolic.marketing',
   },
   {
     title: 'Payfare',
     img: require('../../assets/websites/payfare.png'),
-    website:'https://corp.payfare.com/'
+    website: 'https://corp.payfare.com/',
   },
   {
     title: 'Raydius',
     img: require('../../assets/websites/raydius.png'),
-    website:'https://raydius.ca/'
+    website: 'https://raydius.ca/',
   },
   {
     title: 'Seacan Labs',
     img: require('../../assets/websites/seacan.png'),
-    website:'https://seacanlabs.com'
+    website: 'https://seacanlabs.com',
   },
   {
     title: 'Silverhammer',
     img: require('../../assets/websites/silverhammer.png'),
-    website:'https://silverhammermining.com/'
+    website: 'https://silverhammermining.com/',
   },
   {
     title: 'Smart Street Investing',
     img: require('../../assets/websites/smartstreet.png'),
-    website:'https://smartstreetinvesting.com'
+    website: 'https://smartstreetinvesting.com',
   },
   {
     title: 'Susgrainable',
     img: require('../../assets/websites/susgrainable.png'),
-    website:'https://susgrainable.com/'
+    website: 'https://susgrainable.com/',
   },
   {
     title: 'Terracota',
     img: require('../../assets/websites/terracota.png'),
-    website:'https://www.terracottatummy.com/'
+    website: 'https://www.terracottatummy.com/',
   },
   {
     title: 'Thai Terre',
     img: require('../../assets/websites/thaiterre.png'),
-    website:'https://thaiterre.co.uk/'
+    website: 'https://thaiterre.co.uk/',
   },
   {
     title: 'Trillium Gold',
     img: require('../../assets/websites/trillium.png'),
-    website:'https://www.trilliumgold.com/'
+    website: 'https://www.trilliumgold.com/',
   },
   {
     title: 'Two Brothers Toffe',
     img: require('../../assets/websites/twobrothers.png'),
-    website:'https://twobrotherstoffee.com/'
+    website: 'https://twobrotherstoffee.com/',
   },
   {
     title: 'WeVest',
     img: require('../../assets/websites/wevest.png'),
-    website:'https://wevest.ca/'
+    website: 'https://wevest.ca/',
   },
   {
     title: 'Warda Restaurant',
     img: require('../../assets/websites/warda.png'),
-    website:'https://wardarestaurant.co.uk/'
+    website: 'https://wardarestaurant.co.uk/',
   },
   {
     title: 'Wendell Home Inspection',
     img: require('../../assets/websites/wendellHome.png'),
-    website:'https://wendellhomeinspections.com/'
+    website: 'https://wendellhomeinspections.com/',
   },
   {
     title: 'Wanderfy (Personal Project)',
     img: require('../../assets/websites/wanderfy.png'),
-    website:'https://wanderfy.vercel.app/'
+    website: 'https://wanderfy.vercel.app/',
+  },
+  {
+    title: 'Whales HC',
+    img: require('../../assets/websites/whaleshc.png'),
+    website: 'https://whaleshc.com/',
   },
   {
     title: 'Uno Digital',
     img: require('../../assets/websites/unodigital.png'),
-    website:'https://unodigital.com/'
+    website: 'https://unodigital.com/',
+  },
+  {
+    title: 'Urban Fireplaces',
+    img: require('../../assets/websites/ufp.png'),
+    website: 'https://urbanfp.ca/',
   },
   {
     title: 'Uptown Printer',
     img: require('../../assets/websites/uptown.png'),
-    website:'https://uptownprinters.ca/'
-  }
-]
+    website: 'https://uptownprinters.ca/',
+  },
+];
