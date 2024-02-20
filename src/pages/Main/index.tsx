181d3e6171c5d6ea4f6d39810d4db7a4928b72f7
Wanderson Castro <wandersonce1@hotmail.com>
2024-02-20 12:34:35 -0800

    Fix Name

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,31 +1,30 @@
-import {motion} from 'framer-motion';
-import { Container } from "./styles"
+import { motion } from 'framer-motion';
+import { Container } from './styles';
 
 import Selfie from '../../assets/selfie.png';
 import Particles from '../../components/Particles';
 
 export function Main() {
-
-  return(
+  return (
     <Container>
-    <motion.div 
-    className="center-container"
-    initial={{ opacity: 0 }}
-    animate={{opacity:1}}
-    exit={{ opacity: 0 }}
-    >
-    <div>
-      <img src={Selfie} alt="Wanderson's Selfie" />
-    </div>
-    <div>
-      <h1 className="fadeIn-item">
-      I'm <br />
-      Wanderson <br />
-      Castro
-    </h1>
-    <h3>Front-end Developer</h3>
-    </div>
-  </motion.div>
- </Container>
-  )
+      <motion.div
+        className="center-container"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <div>
+          <img src={Selfie} alt="Wanderson's Selfie" />
+        </div>
+        <div>
+          <h1 className="fadeIn-item">
+            I'm <br />
+            Wanderson <br />
+            Castro
+          </h1>
+          <h3>Front-end Developer</h3>
+        </div>
+      </motion.div>
+    </Container>
+  );
 }
--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -3,90 +3,103 @@ import styled from 'styled-components';
 import backgroundNameImg from '../../assets/coding6.jpg';
 
 export const Container = styled.main`
-display: flex;
-align-items: center;
-justify-content: center;
-width: 100%;
-height: calc(100vh - 200px);
-position:relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: calc(100vh - 200px);
+  position: relative;
 
-.center-container{
-  height:100%;
-  width:100%;
-  display:flex;
-  flex-direction:row;
+  .center-container {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
 
-  div{
-    display:flex;
-    flex-basis:50%;
-    align-items:flex-start;
-    justify-content:center;
-    flex-direction:column;
-    padding:4rem;
+    div {
+      display: flex;
+      flex-basis: 50%;
+      align-items: flex-start;
+      justify-content: center;
+      flex-direction: column;
+      padding: 4rem;
 
-    &:first-child{
-      padding-bottom:0;
-      display:flex;
-      align-items:center;
+      &:first-child {
+        padding-bottom: 0;
+        display: flex;
+        align-items: center;
+      }
+
+      h3 {
+        font-size: 1.6rem;
+      }
     }
 
-    h3{ 
-      font-size:1.6rem;
+    div h1 {
+      font-size: 5rem;
+      font-weight: bold;
+
+      overflow: hidden;
     }
-  }
 
-  div h1{
-    font-size:6rem;
-    font-weight:bold;
-    background: url(${backgroundNameImg})  no-repeat left;
-    background-size: contain;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    overflow:hidden;
-  }
-
-  .fadeIn-item {
-    opacity: 0;
-    animation-name: fadein;
-    animation-duration: 500ms;
-    animation-iteration-count: 1;
-    animation-fill-mode: forwards;
-  }
-
-  @keyframes fadein {
-    0% {
+    .fadeIn-item {
       opacity: 0;
-    }
-  
-    100% {
-      opacity: 1;
-    }
-  }
-}
-@media only screen and (max-width: 810px) {
-  .center-container{
-    flex-direction:column-reverse;
-    div{
-      padding:2rem;
-    }
-    div h1{
-      font-size:6rem;
-    }
-  }
-}
-
-@media only screen and (max-width: 640px) {
-  height: 100%;
-  .center-container{
-    div h1{
-      font-size:3.5rem;
+      animation-name: fadein;
+      animation-duration: 500ms;
+      animation-iteration-count: 1;
+      animation-fill-mode: forwards;
     }
 
-    div{
-     img{
-       max-height:400px;
-     }
+    @keyframes fadein {
+      0% {
+        opacity: 0;
+      }
+
+      100% {
+        opacity: 1;
+      }
     }
   }
-}
-`
+
+  @media only screen and (max-width: 1400px) {
+    .center-container {
+      div {
+        width: 100%;
+        display: flex;
+      }
+      div h1 {
+        width: 100%;
+        font-size: 4rem;
+        line-height: 1em;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 810px) {
+    .center-container {
+      flex-direction: column-reverse;
+      div {
+        padding: 2rem;
+      }
+      div h1 {
+        font-size: 4rem;
+        line-height: 1.5em;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 640px) {
+    height: 100%;
+    .center-container {
+      div h1 {
+        font-size: 3.5rem;
+      }
+
+      div {
+        img {
+          max-height: 400px;
+        }
+      }
+    }
+  }
+`;
